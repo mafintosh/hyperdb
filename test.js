46dacc7c46f93ea4c7c74ba87dd46b274dcdfefb
Emil Bay <github@tixz.dk>
2017-05-24 16:53:05 +0200

    Add overwrite test

--- test.js
+++ test.js
@@ -20,11 +20,36 @@ test('put/get', function (assert) {
   db.put('foo', 'bar', function (err) {
     assert.error(err)
 
-    db.get('foo', function (err, val) {
+    db.get('foo', function (err, node) {
       assert.error(err)
-      assert.ok(val)
+      assert.equals(node.value, 'bar')
 
       assert.end()
+    })
+  })
+})
+
+test('put/get', function (assert) {
+  var db = hyperdb([
+    hypercore(ram, {valueEncoding: 'json'})
+  ])
+
+  db.put('foo', 'bar', function (err) {
+    assert.error(err)
+
+    db.get('foo', function (err, node) {
+      assert.error(err)
+      assert.ok(node.value, 'bar')
+
+      db.put('foo', 'baz', function (err) {
+        assert.error(err)
+
+        db.get('foo', function (err, node) {
+          assert.error(err)
+          assert.equals(node.value, 'baz')
+          assert.end()
+        })
+      })
     })
   })
 })
