fd6034421eb4d1763260d80a636b0e7cb17cba0a
Mathias Buus <mathiasbuus@gmail.com>
2018-03-15 03:11:24 +0100

    add hash collision test. (thanks to @dcposch for finding one)

---
+++ test/collisions.js
@@ -0,0 +1,45 @@
+var tape = require('tape')
+var create = require('./helpers/create')
+
+tape('two keys with same siphash', function (t) {
+  t.plan(2 + 2)
+
+  var db = create.one()
+
+  db.put('idgcmnmna', 'a', function () {
+    db.put('mpomeiehc', 'b', function () {
+      db.get('idgcmnmna', function (err, node) {
+        t.error(err, 'no error')
+        t.same(node.value, 'a')
+      })
+      db.get('mpomeiehc', function (err, node) {
+        t.error(err, 'no error')
+        t.same(node.value, 'b')
+      })
+    })
+  })
+})
+
+tape('two keys with same siphash (iterator)', function (t) {
+  var db = create.one()
+
+  db.put('idgcmnmna', 'a', function () {
+    db.put('mpomeiehc', 'b', function () {
+      var ite = db.iterator()
+
+      ite.next(function (err, node) {
+        t.error(err, 'no error')
+        t.same(node.value, 'a')
+      })
+      ite.next(function (err, node) {
+        t.error(err, 'no error')
+        t.same(node.value, 'b')
+      })
+      ite.next(function (err, node) {
+        t.error(err, 'no error')
+        t.same(node, null)
+        t.end()
+      })
+    })
+  })
+})
