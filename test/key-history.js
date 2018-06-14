74a8e1ebd7416246224722ac58fa7e9347a68e82
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-06-14 21:33:04 +1000

    fix a few issues and add extra test cases

--- test/key-history.js
+++ test/key-history.js
@@ -1,6 +1,7 @@
 var tape = require('tape')
 var toStream = require('nanoiterator/to-stream')
 
+var replicate = require('./helpers/replicate')
 var create = require('./helpers/create')
 var put = require('./helpers/put')
 var run = require('./helpers/run')
@@ -13,6 +14,7 @@ tape('empty db', (t) => {
     t.end
   )
 }, { timeout: 1000 })
+
 tape('single feed', (t) => {
   var db = create.one()
   run(
@@ -23,6 +25,19 @@ tape('single feed', (t) => {
     ], cb),
     cb => testHistory(t, db, 'hello', ['world', 'welt'], cb),
     cb => testHistory(t, db, 'null', ['void'], cb),
+    t.end
+  )
+}, { timeout: 1000 })
+
+tape('single feed (same value)', (t) => {
+  var db = create.one()
+  run(
+    cb => put(db, [
+      { key: 'hello', value: 'welt' },
+      { key: 'hello', value: 'darkness' },
+      { key: 'hello', value: 'world' }
+    ], cb),
+    cb => testHistory(t, db, 'hello', ['world', 'darkness', 'welt'], cb),
     t.end
   )
 }, { timeout: 1000 })
@@ -45,16 +60,80 @@ tape('two feeds', (t) => {
   })
 }, { timeout: 1000 })
 
+tape('two feeds with conflict', (t) => {
+  create.two((db1, db2, replicate) => {
+    run(
+      cb => put(db1, [
+        { key: 'hello', value: 'welt' },
+        { key: 'null', value: 'void' }
+      ], cb),
+      cb => put(db2, [
+        { key: 'hello', value: 'world' }
+      ], cb),
+      replicate,
+      cb => testHistory(t, db1, 'hello', [['world', 'welt']], cb),
+      t.end
+    )
+  })
+}, { timeout: 1000 })
+
+tape('three feeds with conflict', (t) => {
+  create.three((db1, db2, db3, replicateAll) => {
+    run(
+      cb => put(db1, [
+        { key: 'hello', value: 'welt' },
+        { key: 'null', value: 'void' }
+      ], cb),
+      cb => replicate(db1, db2, cb),
+      cb => put(db2, [
+        { key: 'hello', value: 'world' }
+      ], cb),
+      cb => replicate(db1, db2, cb),
+      cb => put(db3, [
+        { key: 'hello', value: 'again' }
+      ], cb),
+      replicateAll,
+      cb => testHistory(t, db1, 'hello', [['world', 'again'], 'welt'], cb),
+      t.end
+    )
+  })
+}, { timeout: 1000 })
+
+tape('three feeds with all conflicting', (t) => {
+  create.three((db1, db2, db3, replicateAll) => {
+    run(
+      cb => put(db1, [
+        { key: 'hello', value: 'welt' },
+        { key: 'null', value: 'void' }
+      ], cb),
+      cb => put(db2, [
+        { key: 'hello', value: 'world' }
+      ], cb),
+      cb => put(db3, [
+        { key: 'hello', value: 'again' }
+      ], cb),
+      replicateAll,
+      cb => testHistory(t, db1, 'hello', [['world', 'again', 'welt']], cb),
+      t.end
+    )
+  })
+}, { timeout: 1000 })
+
 function testHistory (t, db, key, expected, cb) {
   const results = expected.slice(0)
   const stream = toStream(keyHistory(db, key))
   stream.on('data', (data) => {
-    console.log(data)
-    console.log(data[0].value)
-    t.same(data[0].value, results.shift())
+    var expected = results.shift()
+    // console.log(data, expected)
+    t.notEqual(expected, undefined)
+    if (!Array.isArray(expected)) expected = [expected]
+    t.same(data.length, expected.length)
+    expected.forEach((value, i) => {
+      t.same(data[i].value, value)
+    })
   })
   stream.on('end', () => {
-    console.log('end')
+    // console.log('end')
     t.same(results.length, 0)
     cb()
   })
